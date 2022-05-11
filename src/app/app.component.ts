01c0ec3df9acb13988465fccbb935392fc3683ab
Damian-Teodor Beles <damianteodor.beles@gmail.com>
2022-05-11 17:18:15 +0300

    Add upload functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, switchMap} from "rxjs/operators";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'upload';
+export class AppComponent implements OnInit {
+  files = [];
+
+  endpointBaseUrl = 'https://bdtdis-watermarking.herokuapp.com';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.getFiles().subscribe((files) => { // @ts-ignore
+      this.files = files;
+    });
+  }
+
+  videoUpload(event: any) {
+    this.postFile(event.target.files.item(0)).pipe(switchMap(() => this.getFiles())).subscribe((files) => { // @ts-ignore
+      this.files = files;
+    });
+  }
+
+  postFile(fileToUpload: File) {
+    const fd: FormData = new FormData();
+    fd.append('video', fileToUpload);
+    return this.httpClient.post(`${this.endpointBaseUrl}/upload`, fd);
+  }
+
+  getFiles() {
+    return this.httpClient.get<string[]>(`${this.endpointBaseUrl}/uploads`);
+  }
 }
